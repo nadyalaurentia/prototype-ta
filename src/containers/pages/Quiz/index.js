1ec861fd4c0d89df7fcaecaf0e383f23d7e80ecf
nadyalaurentia <nadyalaurentia@gmail.com>
2023-04-02 14:40:56 +0700

    fix quiz page

--- src/containers/pages/Quiz/index.js
+++ src/containers/pages/Quiz/index.js
@@ -19,8 +19,19 @@ const Quiz = (session) => {
         <div className={contrast}>
             <div className='navbar'></div>
             <div className='content-container'>
+            <p className='read-title'>Pertanyaan Pemahaman</p>
             {listQuestion}    
+            <p className='read-title'>Penilaian Kenyamanan</p>
+            <p className='read-text'>Seberapa nyaman membaca dengan tampilan tersebut?</p>
+                <form>
+                    <div><label><input type="radio" name='likert1' value='1' /> 1 - tidak nyaman </label></div>
+                    <div><label><input type="radio" name='likert2' value='2' /> 2 - kurang nyaman </label></div>
+                    <div><label><input type="radio" name='likert3' value='3' /> 3 - biasa saja </label></div>
+                    <div><label><input type="radio" name='likert4' value='4' /> 4 - cukup nyaman </label></div>
+                    <div><label><input type="radio" name='likert5' value='5' /> 5 - sangat nyaman </label></div>
+                </form>
             </div>
+
             {(session < 8) && <Link to={'/case/'+ (session+1).toString()}><button className='read-btn'>Simpan Jawaban</button></Link>}
             {(session >= 8) && <Link to={'/final'}><button className='read-btn'>Simpan Jawaban</button></Link>}
         </div>
